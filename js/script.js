eb1dc3d06bd0671b9c0f1b52c28cfed80ef8f1af
MrDaveDev <89876750+MrDaveDev@users.noreply.github.com>
2025-02-23 18:37:53 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,9 +5,15 @@ let userName = ''; // Variable to store the Twitch username
 
 // Function to start the OAuth login
 function startOAuthLogin() {
-    console.log('Starting OAuth login...');
-    const url = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=user:read:email`;
-    window.location.href = url; // Redirect to Twitch login page
+    const accessToken = localStorage.getItem('accessToken');
+
+    if(accessToken) {
+        console.log('Access Token Found');
+    } else {
+        console.log('Starting OAuth login...');
+        const url = `https://id.twitch.tv/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=user:read:email`;
+        window.location.href = url; // Redirect to Twitch login page
+    }
 }
 
 // Check for the OAuth token in the URL after redirect
